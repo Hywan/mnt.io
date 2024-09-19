716feb4d127e16b137007aa87eacf27491bf7561
Ivan Enderlin <ivan@mnt.io>
2024-09-19 18:11:34 +0200

    cont

--- content/series/reactive-programming-in-rust/playground/src/main.rs
+++ content/series/reactive-programming-in-rust/playground/src/main.rs
@@ -1,19 +1,40 @@
-use eyeball::Observable;
+use std::task::Poll;
 
-fn main() {
-    smol::block_on(async {
-        let mut observable = Observable::new(7);
+use eyeball_im::{ObservableVector, VectorDiff};
+use futures::stream::StreamExt;
+use macro_rules_attribute::apply;
+use smol::Executor;
+use smol_macros::main;
 
-        let mut subscriber = Observable::subscribe(&observable);
+macro_rules! assert_next_eq {
+    ( $stream:ident, $expr:expr $(,)? ) => {
+        assert_eq!(dbg!($stream.next().await), Some($expr),);
+    };
+}
 
-        dbg!(Observable::get(&observable));
-        dbg!(subscriber.get());
+#[apply(main!)]
+async fn main(_executor: &Executor) {
+    let mut observable = ObservableVector::with_capacity(32);
+    let mut subscriber = observable.subscribe().into_stream();
 
-        Observable::set(&mut observable, 13);
+    // Push ALL THE VALUES!
+    observable.push_back('a');
+    observable.push_back('b');
+    observable.push_back('c');
+    observable.push_back('d');
+    observable.push_back('e');
+    observable.push_back('f');
+    observable.push_back('g');
+    observable.push_back('h');
+    observable.push_back('i');
+    observable.push_back('j');
+    observable.push_back('k');
+    observable.push_back('l');
+    observable.push_back('m');
+    observable.push_back('n');
+    observable.push_back('o');
+    observable.push_back('p');
+    observable.push_back('q');
 
-        dbg!(Observable::get(&observable));
-        dbg!(subscriber.get());
-
-        dbg!(subscriber.next().await);
-    })
+    assert_next_eq!(subscriber, VectorDiff::PushBack { value: 'a' });
 }
